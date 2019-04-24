b33e5bdfefcda72a9857ae206ac3c159fa011b9e
Pradier Antoine <antoine.pradier@ensae.fr>
2019-04-24 14:08:08 +0200

    modified retry_delay in first dag

--- first_dag_datascientest.py
+++ first_dag_datascientest.py
@@ -11,7 +11,7 @@ default_args = {
     'depends_on_past': False,
     'start_date': datetime.now()-timedelta(minutes=5, seconds=10),
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(seconds=15),
     # 'end_date': datetime(2016, 1, 1),
     # 'email': ['airflow@example.com'],
     # 'email_on_failure': False,
