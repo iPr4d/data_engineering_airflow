05af4b0f0db4142760c71ba0d37466930ad5c278
Pradier Antoine <antoine.pradier@ensae.fr>
2019-04-24 13:54:51 +0200

    modified start_date in first dag

--- first_dag_datascientest.py
+++ first_dag_datascientest.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'datascientest',
     'depends_on_past': False,
-    'start_date': datetime.now()-timedelta(minutes=4, seconds=50),
+    'start_date': datetime.now()-timedelta(minutes=5, seconds=10),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
     # 'end_date': datetime(2016, 1, 1),
