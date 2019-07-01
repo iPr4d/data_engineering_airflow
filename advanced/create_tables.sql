859218907880a538c18560afee83f3d1929045e1
Pradier Antoine <antoine.pradier@ensae.fr>
2019-07-01 14:29:42 +0200

    removed deprecated files

--- advanced/create_tables.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE trump_rt;
-DROP TABLE trump_not_rt;
-CREATE TABLE trump_rt (id varchar(255) primary key, text varchar(1000), author varchar(255), date varchar(255));
-CREATE TABLE trump_not_rt (id varchar(255) primary key, text varchar(1000), author varchar(255), date varchar(255));
