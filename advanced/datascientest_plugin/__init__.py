e53aba54b1f9676ae66ad43c1c61e7495e38a0ff
Pradier Antoine <antoine.pradier@ensae.fr>
2019-07-03 15:59:55 +0200

    initialized custom airflow plugin

---
+++ advanced/datascientest_plugin/__init__.py
@@ -0,0 +1,14 @@
+from airflow.plugins_manager import AirflowPlugin
+
+from datascientest_plugin.operators import MySQLToMongoOperator
+
+
+class DatascientestPlugin(AirflowPlugin):
+    name = 'datascientest_plugin'
+    operators = [MySQLToMongoOperator]
+    sensors = []
+    hooks = []
+    executors = []
+    admin_views = []
+    flask_blueprints = []
+    menu_links = []
